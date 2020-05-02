0f62e2a1d6fd3243614ec848e501013ff373b921
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-05-02 09:26:25 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using BaiTap6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly QLNVEntities db = new QLNVEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Search(string search, string option)
+        {
+            if (option == "LastName")
+            {
+                return View(db.Employees.Where(x => x.employeeLastName.Contains(search) || search == null).ToList());
+            }
+            else if (option == "FirstName")
+            {
+                return View(db.Employees.Where(x => x.employeeFirstName.Contains(search) || search == null).ToList());
+            }
+            else
+            {
+                return View(db.Employees.Where(x => x.employeeAddress.Contains(search) || search == null).ToList());
+            }
+
+        }
+    }
+}
